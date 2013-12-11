c4414dc97b623d494ecf0787b046bb9477c7b5b7
Brice Texier <burisu@oneiros.fr>
2013-12-11 23:39:11 +0100

    Adapts to Beardley 1.0.0

--- lib/beardley/xml.rb
+++ lib/beardley/xml.rb
@@ -3,8 +3,10 @@ require "beardley/xml/version"
 module Beardley
   module XML
 
-    def self.classpath
-      return Dir[Pathname.new(__FILE__).dirname.join("..", "..", "vendor", "java", "*.jar")].collect{ |path| Pathname.new(path) }
+    RjbLoader.before_load do |config|
+      Dir[Pathname.new(__FILE__).dirname.join("..", "..", "vendor", "java", "*.jar")].each do |path|
+        config.classpath << File::PATH_SEPARATOR + File.expand_path(path)
+      end
     end
 
   end
--- lib/beardley/xml/version.rb
+++ lib/beardley/xml/version.rb
@@ -1,5 +1,5 @@
 module Beardley
   module XML
-    VERSION = "0.0.0"
+    VERSION = "1.0.0"
   end
 end
