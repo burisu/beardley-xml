03e806b7516efe17202e359e88e280b0e2322692
Brice TEXIER <brice@ekylibre.com>
2017-06-29 13:41:45 +0200

    Adds missing require file

--- lib/beardley/xml.rb
+++ lib/beardley/xml.rb
@@ -1,13 +1,11 @@
-require "beardley/xml/version"
+require 'beardley/xml/version'
 
 module Beardley
   module XML
-
     RjbLoader.before_load do |config|
-      Dir[Pathname.new(__FILE__).dirname.join("..", "..", "vendor", "java", "*.jar")].each do |path|
+      Dir[Pathname.new(__FILE__).dirname.join('..', '..', 'vendor', 'java', '*.jar')].each do |path|
         config.classpath << File::PATH_SEPARATOR + File.expand_path(path)
       end
     end
-
   end
 end
--- lib/beardley/xml/version.rb
+++ lib/beardley/xml/version.rb
@@ -1,5 +1,5 @@
 module Beardley
   module XML
-    VERSION = "1.1.1"
+    VERSION = '1.1.2'.freeze
   end
 end
